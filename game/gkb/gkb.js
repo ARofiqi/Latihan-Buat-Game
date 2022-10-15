c0614fd23de08f0a7debfad513e40951303e4814
ARofiqi <ahmad.rofiqi_ti22@nusaputra.ac.id>
2022-10-15 14:22:03 +0700

    menambahkan fungsi gkb

--- game/gkb/gkb.js
+++ game/gkb/gkb.js
@@ -2,7 +2,9 @@ const opt_player = ["gunting", "kertas", "batu"]
 let player;
 
 function opt(i){
-    alert("Pilih " + opt_player[i])
     player = opt_player[i]
 }
 
+while(true){
+    let bot = Math.floor(Math.random()*3)
+}
