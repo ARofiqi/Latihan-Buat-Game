b2143acda5b228e108fff54e9c62c63f4986e092
ARofiqi <ahmad.rofiqi_ti22@nusaputra.ac.id>
2022-10-15 15:25:26 +0700

    mengubah layout gkb

--- game/gkb/gkb.js
+++ game/gkb/gkb.js
@@ -1,10 +1,33 @@
-const opt_player = ["gunting", "kertas", "batu"]
-let player;
+const text_hasil = document.querySelector(".hasil h1");
+const bot_img = document.querySelector(".bot img");
 
-function opt(i){
-    player = opt_player[i]
-}
+function opt(i) {
+  let player = i;
+  let hasil;
+  // kertas gunting batu
 
-while(true){
-    let bot = Math.floor(Math.random()*3)
+  let bot = Math.floor(Math.random() * 3);
+  if (player == bot) {
+    hasil = "SERI";
+  } else if (player == 0 && bot == 2) {
+    hasil = "MENANG";
+  } else if (player == 1 && bot == 0) {
+    hasil = "MENANG";
+  } else if (player == 2 && bot == 1) {
+    hasil = "MENANG";
+  } else {
+    hasil = "KALAH";
+  }
+  switch(bot){
+    case 0:
+        bot_img.src = "../../img/gkb/kertas.png";
+        break
+    case 1:
+        bot_img.src = "../../img/gkb/gunting.png";
+        break
+    case 2:
+        bot_img.src = "../../img/gkb/batu.png";
+        break
+  }
+  text_hasil.innerHTML = hasil;
 }
